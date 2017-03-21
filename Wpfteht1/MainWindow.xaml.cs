28109cb58acdd35c54b97a4877ab8b1b132e56aa
K9229 <K9229@ICT-421-WS-21>
2017-03-21 13:21:31 +0200

    wpf tehtävii

---
+++ Wpfteht1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfteht1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int trucks = 0;
+        int cars = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void truckButton_Click(object sender, RoutedEventArgs e)
+        {
+            trucks++;
+            truckCount.Text = trucks.ToString();
+        }
+
+        private void carButton_Click(object sender, RoutedEventArgs e)
+        {
+            cars++;
+            carCount.Text = cars.ToString();
+        }
+    }
+}
---
+++ Yksikkötestaus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yksikkötestaus
+{
+    interface ICalculator
+    {
+        int Add(int numero1, int numero2);
+        int Multiply(int numero1, int numero2);
+    }
+    // Luokka, joka toteuttaa ICalculator-rajapinnan.
+    public class Calculator : ICalculator
+    {
+        public int Add(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+        public int Multiply(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kokeillaan luokkaa Calculator.
+            Calculator laskin = new Calculator();
+            int tulos = laskin.Add(10, 20);
+            Console.WriteLine("Yhteenlaskun 10 + 20 tulos on: " + tulos);
+        }
+    }
+}
